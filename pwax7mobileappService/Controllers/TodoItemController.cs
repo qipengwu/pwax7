ae9c7d3c1994abcb75d5ccd70b60539f61397970
Qipeng Wu <wuqip@hotmail.com>
2016-01-19 08:45:52 +0000

    Adding data access to AX7 to mobile service.

--- pwax7mobileappService/Controllers/TodoItemController.cs
+++ pwax7mobileappService/Controllers/TodoItemController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -7,8 +8,13 @@ using Microsoft.Azure.Mobile.Server;
 using pwax7mobileappService.DataObjects;
 using pwax7mobileappService.Models;
 
+using AuthenticationUtility;
+using Microsoft.OData.Client;
+using ODataUtility.Microsoft.Dynamics.DataEntities;
+
 namespace pwax7mobileappService.Controllers
 {
+    [Authorize]
     public class TodoItemController : TableController<TodoItem>
     {
         protected override void Initialize(HttpControllerContext controllerContext)
@@ -40,6 +46,38 @@ namespace pwax7mobileappService.Controllers
         public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
         {
             TodoItem current = await InsertAsync(item);
+            
+            // Begin insert into AX7
+            string ODataEntityPath = ClientConfiguration.Default.UriString + "data";
+
+            Uri oDataUri = new Uri(ODataEntityPath, UriKind.Absolute);
+            /*
+            var ax7context = new Resources(oDataUri);
+
+            
+            ax7context.SendingRequest2 += new EventHandler<SendingRequest2EventArgs>(delegate (object sender, SendingRequest2EventArgs e)
+            {
+                var authenticationHeader = OAuthHelper.GetAuthenticationHeader();
+                e.RequestMessage.SetHeader(OAuthHelper.OAuthHeader, authenticationHeader);
+            });
+            
+            FleetCustomer fleetCustomer = FleetCustomer.CreateFleetCustomer(item.Id, item.Text, 11111, "Wu");
+
+            try
+            {
+                ax7context.AddToFleetCustomers(fleetCustomer);
+
+                // Send updates to the data service.
+                DataServiceResponse response = ax7context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(
+                    "An error occurred when saving changes.", ex);
+            }
+            */
+            // End insert into AX7
+
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
